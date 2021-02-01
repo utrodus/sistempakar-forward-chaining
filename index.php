f6ac577b7928d837884899457208c99b84a2b237
utrodus <come.programmer@gmail.com>
2021-02-01 21:33:17 +0700

    inisasi project

---
+++ index.php
@@ -0,0 +1,420 @@
+<?php include "header.php" ?>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo mr-auto"><a href="index.php">Sistem Pakar</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.php" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#jenispenyakit">Jenis Penyakit Gigi dan Mulut</a></li>
+          <!-- <li class="drop-down"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Deep Drop Down</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li> -->
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+      <a href="#appointment" class="appointment-btn scrollto">Mulai Konsultasi</a>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+    <div class="container">
+      <h1>Sistem Pakar</h1>
+      <h2>Diagnosis Penyakit Gigi dan Mulut Dengan <br>Forward Chainning</h2>
+      <a href="#about" class="btn-get-started scrollto">Mulai Konsultasi</a>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Why Us Section ======= -->
+    <section id="about" class="why-us">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-4 d-flex align-items-stretch">
+            <div class="content">
+              <h3>Tentang Aplikasi</h3>
+              <p>
+                Terdapat banyak faktor yang menyebabkan
+                jarangnya seseorang melakukan pemeriksaan dan
+                konsultasi ke dokter gigi, diantaranya adalah kurangnya
+                rasa perhatian atau kesadaran akan kesehatan gigi dan
+                mulut, mahalnya biaya yang harus dikeluarkan untuk
+                konsultasi, panjangnya antrian yang menyebabkan pasien
+                kurang betah dalam menunggu.
+
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-8 d-flex align-items-stretch">
+            <div class="icon-boxes d-flex flex-column justify-content-center">
+              <div class="row">
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-receipt"></i>
+                    <p> Aplikasi Ini digunakan Untuk membantu pekerjaan dokter dalam melakukan diagnosis penyakit gigi dan mulut serta perawatan
+                    </p>
+                  </div>
+                </div>
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-cube-alt"></i>
+                    <p>Dengan menggunakan sistem
+                      pakar dapat membantu pasien dalam konsultasi dan juga
+                      membantu dokter dalam mendiagnosa suatu penyakit.</p>
+                  </div>
+                </div>
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-images"></i>
+                    <p> Data-data penyakit, gejala dan cara penanganan didapatkan dari wawancara langsung dengan pakar, jurnal dan bahan
+                      dari internet.</p>
+                  </div>
+                </div>
+              </div>
+            </div><!-- End .content-->
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
+
+    <!-- ======= Departments Section ======= -->
+    <section id="jenispenyakit" class="departments">
+      <div class="container">
+
+        <div class="section-title mb-4">
+          <h2>Jenis penyakit gigi dan mulut </h2>
+          <p>
+            Jenis penyakit gigi dan mulut ini sangat
+            banyak, namun pada aplikasi ini hanya terdapat 8
+            penyakit saja.
+          </p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-3">
+            <ul class="nav nav-tabs flex-column">
+              <li class="nav-item">
+                <a class="nav-link active show" data-toggle="tab" href="#tab-1">Gingivitis (Radang gusi)</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tab-2">Karies Gigi (gigi berlubang)</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tab-3">Karang Gigi</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tab-4">Stomatitis</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tab-5">Abses Periodental</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tab-6">Candidas Oral</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tab-7">Trench Mouth</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tab-8">Gigi Sensitif</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-9 mt-5 mt-lg-0">
+            <div class="tab-content">
+              <div class="tab-pane active show" id="tab-1">
+                <div class="row">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Gingivitis (Radang gusi)</h3>
+                    <p class="font-italic">Radang gusi atau gingivitis adalah peradangan pada gusi yang ditandai oleh memerahnya gusi di sekitar akar gigi. Gingivitis terjadi ketika sisa makanan di gigi dan gusi mengeras menjadi plak.
+
+                    <p>
+
+                      Gejala Radang Gusi :
+                    </p>
+                    <ol>
+                      <li>
+                        Bau mulut.
+                      </li>
+                      <li>
+                        Gusi bengkak, merah dan berdarah
+
+                      </li>
+                      <li>
+                        Gingival berkaratin, gaung luka diantara
+                        gigi dan gusi
+
+                      </li>
+                      <li>
+                        Pembesaran limfoid di kepala, leher, atau
+                        rahang
+
+                      </li>
+                      <li>Demam</li>
+                      <li>Nyeri gusi</li>
+                    </ol>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-pane" id="tab-2">
+                <div class="row">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Karies Gigi (gigi berlubang)</h3>
+                    <p class="font-italic">Karies gigi disebabkan oleh sisa-sisa makanan dan gula pada permukaan gigi yang berubah membentuk asam oleh karena bakteri. Sebelum hal itu terjadi, mari lakukan tindakan pencegahan sedini mungkin.
+                    <p>
+                      Gejala Karies Gigi :
+                    </p>
+                    <ol>
+                      <li>
+                        Bau mulut.
+                      </li>
+                      <li>
+                        Gusi bengkak, merah dan berdarah
+                      </li>
+                      <li>
+                        Gingival berkaratin, gaung luka diantara
+                        gigi dan gusi
+
+                      </li>
+                      <li>
+                        Pembesaran limfoid di kepala, leher, atau
+                        rahang
+                      </li>
+                      <li>Demam</li>
+                      <li>Nyeri gusi</li>
+                    </ol>
+                  </div>
+
+                </div>
+              </div>
+              <div class="tab-pane" id="tab-3">
+                <div class="row">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Karang Gigi</h3>
+                    <p class="font-italic">Karang gigi adalah kondisi di mana terdapat lapisan seperti kotoran yang terletak di gigi dan sulit dihilangkan meski telah dibersihkan atau disikat. Karang gigi disebabkan oleh adanya plak yang mengeras dan tidak mendapat penanganan. Plak itu sendiri adalah lapisan licin dan tipis pada gigi yang terbentuk akibat adanya sisa-sisa makanan yang tertinggal di gigi.
+                    <p>
+                      Gejala Karang Gigi :
+                    </p>
+                    <ol>
+                      <li>
+                        Bau mulut.
+                      </li>
+                      <li>
+                        Gusi bengkak, merah dan berdarah
+                      </li>
+                      <li>
+                        Gingival berkaratin, gaung luka diantara
+                        gigi dan gusi
+
+                      </li>
+                      <li>
+                        Pembesaran limfoid di kepala, leher, atau
+                        rahang
+                      </li>
+                      <li>Demam</li>
+                      <li>Nyeri gusi</li>
+                    </ol>
+                  </div>
+
+                </div>
+              </div>
+              <div class="tab-pane" id="tab-4">
+                <div class="row">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Stomatitis</h3>
+                    <p class="font-italic">Stomatitis adalah radang yang terjadi pada mukosa mulut, biasanya berupa bercak putih kekuningan.</p>
+                    <p class="font-italic">Stomatitis dapat disebabkan oleh beberapa faktor, antara lain defisiensi vitamin seperti zat besi, asam folat, vitamin B12 atau B kompleks, psikologis, trauma, endokrin, herediter, alergi, imunologi, hingga tergigit, luka ketika menyikat gigi, alergi terhadap makanan ataupun adanya infeksi oleh bakteri.</p>
+                    <p>
+                      Gejala Stomatitis :
+                    </p>
+                    <ol>
+                      <li>
+                        Bau mulut.
+                      </li>
+                      <li>
+                        Gusi bengkak, merah dan berdarah
+                      </li>
+                      <li>
+                        Gingival berkaratin, gaung luka diantara
+                        gigi dan gusi
+
+                      </li>
+                      <li>
+                        Pembesaran limfoid di kepala, leher, atau
+                        rahang
+                      </li>
+                      <li>Demam</li>
+                      <li>Nyeri gusi</li>
+                    </ol>
+                  </div>
+
+                </div>
+              </div>
+              <div class="tab-pane" id="tab-5">
+                <div class="row">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Abses Periodental</h3>
+                    <p class="font-italic">Abses gigi adalah terbentuknya kantung atau benjolan berisi nanah pada gigi. Abses gigi disebabkan oleh infeksi bakteri. Kondisi ini bisa muncul di sekitar akar gigi maupun di gusi.
+                    </p>
+                    <p>
+                      Gejala Karang Gigi :
+                    </p>
+                    <ol>
+                      <li>
+                        Bau mulut.
+                      </li>
+                      <li>
+                        Gusi bengkak, merah dan berdarah
+                      </li>
+                      <li>
+                        Gingival berkaratin, gaung luka diantara
+                        gigi dan gusi
+
+                      </li>
+                      <li>
+                        Pembesaran limfoid di kepala, leher, atau
+                        rahang
+                      </li>
+                      <li>Demam</li>
+                      <li>Nyeri gusi</li>
+                    </ol>
+                  </div>
+
+                </div>
+              </div>
+              <div class="tab-pane" id="tab-6">
+                <div class="row">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Candidas Oral</h3>
+                    <p class="font-italic">Candidiasis atau kandidiasis adalah infeksi jamur yang disebabkan oleh jamur Candida albicans. Infeksi jamur ini biasanya terjadi di kulit, mulut, dan organ intim. Jika tidak mendapatkan penanganan, infeksi akibat jamur ini bisa menyebar ke bagian tubuh lain, seperti usus, ginjal, jantung, dan otak.
+                    <p>
+                      Gejala Karang Gigi :
+                    </p>
+                    <ol>
+                      <li>
+                        Bau mulut.
+                      </li>
+                      <li>
+                        Gusi bengkak, merah dan berdarah
+                      </li>
+                      <li>
+                        Gingival berkaratin, gaung luka diantara
+                        gigi dan gusi
+
+                      </li>
+                      <li>
+                        Pembesaran limfoid di kepala, leher, atau
+                        rahang
+                      </li>
+                      <li>Demam</li>
+                      <li>Nyeri gusi</li>
+                    </ol>
+                  </div>
+
+                </div>
+              </div>
+              <div class="tab-pane" id="tab-7">
+                <div class="row">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Trench Mouth</h3>
+                    <p class="font-italic">Trench mouth adalah jenis dari gingivitis parah yang menyebabkan rasa sakit, infeksi, dan perdarahan pada gusi. Trench mouth umum terjadi di negara-negara berkembang yang memiliki gizi serta kondisi hidup yang buruk.
+                    <p>
+                      Gejala Karang Gigi :
+                    </p>
+                    <ol>
+                      <li>
+                        Bau mulut.
+                      </li>
+                      <li>
+                        Gusi bengkak, merah dan berdarah
+                      </li>
+                      <li>
+                        Gingival berkaratin, gaung luka diantara
+                        gigi dan gusi
+
+                      </li>
+                      <li>
+                        Pembesaran limfoid di kepala, leher, atau
+                        rahang
+                      </li>
+                      <li>Demam</li>
+                      <li>Nyeri gusi</li>
+                    </ol>
+                  </div>
+
+                </div>
+              </div>
+              <div class="tab-pane" id="tab-8">
+                <div class="row">
+                  <div class="col-lg-8 details order-2 order-lg-1">
+                    <h3>Gigi Sensitif</h3>
+                    <p class="font-italic">Gigi sensitif adalah kondisi ketika muncul sensasi ngilu dan nyeri pada gigi. Sensasi tersebut muncul sebagai respons terhadap sejumlah kondisi, misalnya akibat makan atau minum yang panas atau dingin. Gigi sensitif dapat terjadi sementara atau dalam jangka panjang, baik pada satu gigi maupun beberapa gigi.
+                    <p>
+                      Gejala Karang Gigi :
+                    </p>
+                    <ol>
+                      <li>
+                        Bau mulut.
+                      </li>
+                      <li>
+                        Gusi bengkak, merah dan berdarah
+                      </li>
+                      <li>
+                        Gingival berkaratin, gaung luka diantara
+                        gigi dan gusi
+
+                      </li>
+                      <li>
+                        Pembesaran limfoid di kepala, leher, atau
+                        rahang
+                      </li>
+                      <li>Demam</li>
+                      <li>Nyeri gusi</li>
+                    </ol>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Departments Section -->
+
+
+  </main><!-- End #main -->
+
+
+
+  <!-- <div id="preloader"></div> -->
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+  <?php include "footer.php" ?>
